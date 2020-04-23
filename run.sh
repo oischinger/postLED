cc16aa64f7d49530b92bfe38dec32effa40f4a52
Hans Oischinger <hans.oischinger@gmail.com>
2020-04-23 21:17:28 +0200

    remove bcm2835 modules on start

--- run.sh
+++ run.sh
@@ -7,6 +7,8 @@ HOST=$(jq --raw-output ".mqtt_host_port" $CONFIG_PATH)
 
 cd /usr/src/postled
 rmmod snd_bcm2835
+rmmod bcm2835_v4l2
+rmmod bcm2835_codec
 setcap 'cap_sys_nice=eip' postled
 echo Starting PostLED
 ./postled -h $HOST -u $USERNAME -p $PASSWORD --led-rows=16 --led-cols=32
